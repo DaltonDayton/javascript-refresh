b855c93738489a61e2d69f57418ba75b6585282e
Dalton Dayton <daltondayton1@gmail.com>
2023-06-18 13:18:39 -0600

    Functions as Values

--- assets/scripts/App.js
+++ assets/scripts/App.js
@@ -122,3 +122,27 @@
 // };
 
 // console.log(extendedUser);
+
+// // ===================
+// // Functions as Values
+// // ===================
+
+// function handleTimeout() {
+//   console.log("Timed out!");
+// }
+
+// const handleTimeout2 = () => {
+//   console.log("Timed out ... again!");
+// };
+
+// setTimeout(handleTimeout, 2000);
+// setTimeout(handleTimeout2, 3000);
+// setTimeout(() => {
+//   console.log("More timing out...");
+// }, 4000);
+
+// function greeter(greetFn) {
+//   greetFn();
+// }
+
+// greeter(() => console.log("Hi"));
