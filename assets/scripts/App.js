f8aa53e1a7a18f21bff5440926e22ee300260304
Dalton Dayton <daltondayton1@gmail.com>
2023-06-18 12:49:09 -0600

    array methods

--- assets/scripts/App.js
+++ assets/scripts/App.js
@@ -1,30 +1,41 @@
-// import default exports
-import apiKeyDefault from "./utils.js";
+// // import default exports
+// import apiKeyDefault from "./utils.js";
 
-// Import multiple non-default exports (with alias)
-import { apiKey, abc as content } from "./utils.js";
+// // Import multiple non-default exports (with alias)
+// import { apiKey, abc as content } from "./utils.js";
 
-// Import everything
-import * as utils02 from "./utils02.js";
+// // Import everything
+// import * as utils02 from "./utils02.js";
 
-console.log("apiKeyDefault: " + apiKeyDefault);
-console.log("apiKey: " + apiKey);
-console.log("abc as content: " + content);
+// console.log("apiKeyDefault: " + apiKeyDefault);
+// console.log("apiKey: " + apiKey);
+// console.log("abc as content: " + content);
 
-console.log("utils02.default: " + utils02.default);
-console.log("utils02.apiKey: " + utils02.apiKey);
-console.log("utils02.zxy: " + utils02.zxy);
+// console.log("utils02.default: " + utils02.default);
+// console.log("utils02.apiKey: " + utils02.apiKey);
+// console.log("utils02.zxy: " + utils02.zxy);
 
-class User {
-  constructor(name, age) {
-    this.name = name;
-    this.age = age;
-  }
+// class User {
+//   constructor(name, age) {
+//     this.name = name;
+//     this.age = age;
+//   }
 
-  greet() {
-    console.log("Hi");
-  }
-}
+//   greet() {
+//     console.log("Hi");
+//   }
+// }
 
-const user1 = new User("Max", 30);
-console.log(user1);
+// const user1 = new User("Max", 30);
+// console.log(user1);
+
+const hobbies = ["Sports", "Cooking", "Reading"];
+hobbies.push("Working");
+
+const index = hobbies.findIndex((hobby) => hobby === "Cooking");
+
+console.log(index);
+
+const editedHobbies = hobbies.map((item) => ({ hobby: item }));
+
+console.log(editedHobbies);
