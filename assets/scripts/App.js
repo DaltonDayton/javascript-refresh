156ba10e89c366df22bd55be1c26cabc58a963e8
Dalton Dayton <daltondayton1@gmail.com>
2023-06-18 13:06:15 -0600

    Destructuring and spread operator

--- assets/scripts/App.js
+++ assets/scripts/App.js
@@ -1,3 +1,7 @@
+// =============
+// Import Export
+// =============
+
 // // import default exports
 // import apiKeyDefault from "./utils.js";
 
@@ -15,6 +19,10 @@
 // console.log("utils02.apiKey: " + utils02.apiKey);
 // console.log("utils02.zxy: " + utils02.zxy);
 
+// ===============
+// Objects Classes
+// ===============
+
 // class User {
 //   constructor(name, age) {
 //     this.name = name;
@@ -29,13 +37,88 @@
 // const user1 = new User("Max", 30);
 // console.log(user1);
 
-const hobbies = ["Sports", "Cooking", "Reading"];
-hobbies.push("Working");
+// ======
+// Arrays
+// ======
 
-const index = hobbies.findIndex((hobby) => hobby === "Cooking");
+// const hobbies = ["Sports", "Cooking", "Reading"];
+// hobbies.push("Working");
 
-console.log(index);
+// const index = hobbies.findIndex((hobby) => hobby === "Cooking");
 
-const editedHobbies = hobbies.map((item) => ({ hobby: item }));
+// console.log(index);
 
-console.log(editedHobbies);
+// const editedHobbies = hobbies.map((item) => ({ hobby: item }));
+
+// console.log(editedHobbies);
+
+// ==============
+// Desctructuring
+// ==============
+
+//// Desctructuring Arrays
+
+// const userNameData = ["Dalton", "Dayton"];
+
+// const firstName = userNameData[0];
+// const lastName = userNameData[1];
+
+// const [firstName, lastName] = ["Dalton", "Dayton"];
+
+// console.log(firstName);
+// console.log(lastName);
+
+//// Desctructuring Objects
+
+// const user = {
+//   name: "Dalton",
+//   age: 28,
+// }
+
+// const name = user.name;
+// const age = user.age;
+
+// const { name: userName, age } = {
+//   name: "Dalton",
+//   age: 28,
+// };
+
+// console.log(userName);
+// console.log(age);
+
+//// Desctructuring function parameter lists
+
+// function storeOrder(order) {
+//   localStorage.setItem("id", order.id);
+//   localStorage.setItem("currency", order.currency);
+// }
+
+// function storeOrderDestructured({ id, currency }) {
+//   // destructuring
+//   localStorage.setItem("id", id);
+//   localStorage.setItem("currency", currency);
+// }
+
+// storeOrderDestructured({ id: 5, currency: "USD", amount: 15.99 }); // one argument / value!
+
+// ===============
+// Spread Operator
+// ===============
+
+// const hobbies = ["Sports", "Cooking"];
+// const user = {
+//   name: "Max",
+//   age: 34,
+// };
+
+// const newHobbies = ["Reading"];
+
+// const mergedHobbies = [...hobbies, ...newHobbies];
+// console.log(mergedHobbies);
+
+// const extendedUser = {
+//   isAdmin: true,
+//   ...user,
+// };
+
+// console.log(extendedUser);
